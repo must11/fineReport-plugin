ebf717e5a0c63ec030e1d227ffb3d122913e8649
must11 <1035462362@qq.com>
2022-09-23 09:44:46 +0800

    添加第一个类，开始开发

---
+++ src/main/java/com/git/must11/plugin/panel/JsonContentPanel.java
@@ -0,0 +1,10 @@
+package com.git.must11.plugin.panel;
+
+import com.fr.design.dialog.BasicPane;
+
+public class JsonContentPanel extends BasicPane {
+    @Override
+    protected String title4PopupWindow() {
+        return "json";
+    }
+}
